e8440215594088e1a2591f3268b56ad25635234b
sgp62 <sgp62@cornell.edu>
2021-02-17 01:37:14 -0500

    done except graph

--- hw1/files/sgp62_hw1_1.c
+++ hw1/files/sgp62_hw1_1.c
@@ -18,7 +18,7 @@ cache access time by different strides
 (6) use gnuplot to plot timing results, "gnuplot cahce_plots.gp"
     where the script cache_plots.gp is provided
 
-    compile: gcc -std=gnu99 -o sgp62_hw1_1 sgp62_hw1_1.c -lm
+    compile: gcc -std=gnu99 -o sgp62_hw1_1 sgp62_hw1_1.c
     run: ./sgp62_hw1_cache                          
 */
 #define _POSIX_C_SOURCE 199309L //Used since multiple structs and macros were undefined without it, such as CLOCK_MONOTONIC and timespec
@@ -29,8 +29,8 @@ cache access time by different strides
 
 #define K 		10	        // run each stride K times
 #define BILLION 	1000000000
-#define min_exp         2              // smallest array tested
-#define max_exp         5              // use as large max_exp as 
+#define min_exp         10              // smallest array tested
+#define max_exp         16              // use as large max_exp as 
                                         // the system will tolerate
 
 int main(int argc, char **argv) {
@@ -38,9 +38,12 @@ int main(int argc, char **argv) {
 // define MIN_SIZE and MAZ_SIZE
   int MAX_SIZE = 1 << max_exp;
   int MIN_SIZE = 1 << min_exp;
+  int logstride = 0;
+  int arrayindex = 0;
+  int num_arrays = max_exp-min_exp;
 
 // for starting and stopping timers
-  struct timespec start, end; //,temp_time; 
+  struct timespec start, end, ntime;
 
 // dynamic memory allocation for A
   float *A;
@@ -52,7 +55,7 @@ int main(int argc, char **argv) {
 
 // dynamic memory allocation for T
   float *T;
-  T = (float *) malloc(MAX_SIZE*sizeof(float));
+  T = (float *) malloc((max_exp*(num_arrays+2))*sizeof(float));
   if(!T) {
     printf("Memory not allocated, malloc returns NULL!\n");
     exit(1);
@@ -67,50 +70,65 @@ int main(int argc, char **argv) {
   clock_getres(CLOCK_MONOTONIC, &start);
   printf("resolution of CLOCK_MONOTONIC is %ld ns\n", start.tv_nsec);
 
-  //float Time1 = clock_gettime(CLOCK_MONOTONIC, &start);
-
 	
   // Main loop,  double the array length starting from min length
-  for(int n = MIN_SIZE; n <= MAX_SIZE; n += n){ //NOTE: what is for array A[0:n-1]? For each of the arrays? Each array is A at a different n, as n goes from MIN_SIZE to MAX_SIZE
+  for(int n = MIN_SIZE; n <= MAX_SIZE; n += n){
+    arrayindex++;
+    logstride = 0;
     //Double the stride
     for(int s = 1; s <= MAX_SIZE; s += s){
+      //Create log of stride index column
+      if(arrayindex == 1 && logstride < max_exp){
+        *(T+logstride*num_arrays) = logstride;
+        fprintf(tp, "%1.3e, ", *(T+logstride*num_arrays));
+        //printf("%.3e ", *(T+logstride*num_arrays));
+      }
+      logstride++;
+      //perform operations if stride length is valid (otherwise populate array with 0)
       if(s <= n/2){
         //start the timer
-        float Tstart = clock_gettime(CLOCK_MONOTONIC, &start);
-        //printf("%.6f \n", Tstart);
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
         // repeat K times the current stride s for averaging
         for(int j = 0; j < K*s; j++) {
           for(int i = 0; i < n; i+=s) {
             //Access A[i]
-            *(A+i) += 0;
+            *(A+i) += 1;
           }
         }
         //Stop Timer, and
         // compute the average access time in K repetitions
-        *(T+n*MAX_SIZE) = log2f(n);
-        //printf("%1.3e ", *(T + n*MAX_SIZE + s));
-        *(T + n*MAX_SIZE + s) = (clock_gettime(CLOCK_MONOTONIC, &end) - Tstart) / (n*K);
-        //printf("%1.3e ", *(T + n*MAX_SIZE + s));
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        ntime.tv_sec = end.tv_sec - start.tv_sec;
+        ntime.tv_nsec = end.tv_nsec - start.tv_nsec;
+        float diff = ntime.tv_sec * BILLION + ntime.tv_nsec;
+        float avgdiff = diff / (n * K);
+        //store time value in array with pointers
+        *(T + logstride*num_arrays + arrayindex) = avgdiff;
       }
       else{
         // Fill in zeroes
-        *(T + n*MAX_SIZE + s) = 0.00;
-        //printf("%1.3e ", *(T + n*MAX_SIZE + s));
+        *(T + logstride*num_arrays + arrayindex) = 0.00;
       }
+      //write to file
+      fprintf(tp, "%1.3e, ", *(T + logstride*num_arrays + arrayindex));
     } // end of loop over strides
+    fprintf(tp, "\n");
+    //printf("\n");
   } // end of loop over subarrays
-/*
-  float Time2 = clock_gettime(CLOCK_MONOTONIC, &end);
-  temp_time.tv_sec = end.tv_sec - start.tv_sec;
-  temp_time.tv_nsec = end.tv_nsec - start.tv_nsec;
-  long diff = temp_time.tv_sec * 1000000000 + temp_time.tv_nsec;
-  printf("%.3f \n", (double) diff);
-*/
 
+//OH Notes:
+//Double pointer in part 3, possibly static tile declaration
+//
 
-// write to file
-  fwrite(&T, sizeof(T), 1, tp);
+//write to file
 
+  // for(int i = 0; i < max_exp; i++){
+  //   for(int j = 0; j < num_arrays; j++){
+  //     fprintf(tp, "%1.3e, ", *(T+i*num_arrays+j));
+  //   }
+  //   fprintf(tp, "\n");
+  // }
 
 
 
